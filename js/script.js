e8ab7bc5d806c5f085dfea1168be32eeabc18a8d
I1yaR <ilyatigrov10@gmail.com>
2022-11-16 22:38:37 +0300

    h5_1

--- js/script.js
+++ js/script.js
@@ -48,5 +48,39 @@ button1.onclick = function t1 () {
   for (let i = 0; i < 50; i++) {
    console.log(i)
   }
-
 }
+
+const button2 = document.querySelector(".button-2")
+const out2 = document.querySelector(".out-2")
+button2.onclick = function t2 () {
+    for (let i = 0;i < 122; i= i + 2) {
+     out2.value += i + " "
+    }
+}
+
+const button3 = document.querySelector(".button-3")
+const out3 = document.querySelector(".out-3")
+button3.onclick = function t3 () {
+    for (let i = 25;i > 0; i--) {
+        out3.value += i + " "
+    }
+}
+
+const button4 = document.querySelector(".button-4")
+const out4 = document.querySelector(".out-4")
+button4.onclick = function t4 () {
+    for (let i = 77; i > 35;i--) {
+        out4.value += i + "_"
+    }
+}
+
+const button5 = document.querySelector(".button-5")
+const out15 = document.querySelector(".out-5")
+button5.onclick = function t5 () {
+    for(let i = 1; i < 17;i++) {
+        if (6 % 3 === 0) {
+            out15.value += i + "_**"
+        } else  {
+            out15.value += i + "_*"
+        }
+    }
